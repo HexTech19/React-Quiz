941a0a9c1a5bb86751b471d37287a669b2912aed
Onyedikachi Nwanguma <nwangumabimma@gmail.com>
2024-07-23 00:41:43 +0100

    v1

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,16 +1,27 @@
 import { useEffect } from "react";
 
 function Timer({ dispatch, secondsRemaining }) {
+  const mins = Math.floor(secondsRemaining / 60);
+  const secs = secondsRemaining % 60;
+
   useEffect(
     function () {
-      setInterval(() => {
+      const id = setInterval(() => {
         dispatch({ type: "tick" });
       }, 1000);
+
+      return () => clearInterval(id);
     },
     [dispatch]
   );
 
-  return <div className="timer">{secondsRemaining}</div>;
+  return (
+    <div className="timer">
+      {mins < 10 && "0"}
+      {mins}:{secs < 10 && "0"}
+      {secs}
+    </div>
+  );
 }
 
 export default Timer;
